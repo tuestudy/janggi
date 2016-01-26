bb72014b2c01a767bc4e910e04afd4c3a2a9327c
dgoon <deadbeef@dgoon.net>
2016-01-26 12:02:21 +0000

    보드 상태에 맞게 변환된 이름을 출력

--- core/janggi.py
+++ core/janggi.py
@@ -6,21 +6,23 @@
     2: ...
     
 """
-NAMES = ['_']   # 나중에 추가
+NAMES = [u'__', u'Kung-a', u'Cha-a', u'Po-a', u'Ma-a', u'Sang-a', u'Byung-a', u'Sa-a', u'Kung-b', u'Cha-b', u'Po-b', u'Ma-b', u'Sang-b', u'Sa-b', u'Jol-b']   # 나중에 추가
+assert(len(NAMES) == 7 + 7 + 1)
 
 def code2name(c):
-    if c >= len(NAMES): # NAMES 가 다 준비되면 지워도 된다
-        return '_'
-
-    return NAMES[c]
+    ret = "[%7s]" % NAMES[c]
+    return ret
 
 class Janggi(object):
     def __init__(self):
         self.board = [[0]*9 for _ in range(10)]
 
     def __repr__(self):
-        return '\n'.join(' '.join(map(lambda x: "%4s" % x, row)) for row in self.board)
+        return '\n'.join(' '.join(map(code2name, row)) for row in self.board)
 
 if '__main__' == __name__:
     janggi = Janggi()
+    print(janggi)
+    print()
+    janggi.board[3][4] = 3
     print(janggi)
