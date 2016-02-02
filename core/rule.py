26bcee9a134492f4edc7c454431d0372ce8af560
dgoon <deadbeef@dgoon.net>
2016-02-02 11:23:18 +0000

    갈 수 있는 위치 후보를 리턴하는 함수의 가정에 대한 주석 업데이트

--- core/rule.py
+++ core/rule.py
@@ -2,7 +2,7 @@
 from data import code2name, name2code, MAX_ROW, MAX_COL, \
         is_valid_code, is_valid_coordinates
 
-# 현재 위치와 기물을 받아서 다음에 갈 수 있는 위치 목록을 리턴
+# 판이 비어있다고 가정하고, 현재 위치와 기물을 받아서 다음에 갈 수 있는 위치 목록을 리턴
 def next_possible_coordinates(current_row, current_col, code):
     assert(is_valid_coordinates(current_row, current_col))
 
