46f590a58f4456528f8290ada563410acdd3cc6a
Jeong-Min Lee <falsetru@gmail.com>
2016-04-12 20:06:07 +0900

    Janggi.move(old_pos, new_pos), Janggi.delete(pos)

--- core/janggi.py
+++ core/janggi.py
@@ -3,9 +3,11 @@ from data import A_INITIAL_STATE, B_INITIAL_STATE, CODE_NAMES, code2name, name2c
 from helper import create_empty_board, board_state
 
 
+EMPTY = 0
+
 class Janggi(object):
     def __init__(self):
-        self.board = [[0]*9 for _ in range(10)]
+        self.board = [[EMPTY]*9 for _ in range(10)]
 
     def __repr__(self):
         return board_state(self.board)
@@ -14,6 +16,19 @@ class Janggi(object):
         self.board = create_empty_board()
         for row, col, code in A_INITIAL_STATE+B_INITIAL_STATE:
             self.board[row][col] = code
+
+    def move(self, old_pos, new_pos):
+        row1, col1 = old_pos
+        row2, col2 = new_pos
+        assert self.board[row1][col1] != EMPTY
+        self.board[row2][col2] = self.board[row1][col1]
+        self.board[row1][col1] = EMPTY
+
+    def delete(self, pos):
+        row, col = pos
+        assert self.board[row][col] != EMPTY
+        self.board[row][col] = EMPTY
+
 
 if '__main__' == __name__:
     janggi = Janggi()
