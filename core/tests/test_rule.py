3d8a818839c23cdb5274cff1fcec0bb706fa8788
Jeong-Min Lee <falsetru@gmail.com>
2016-04-19 20:37:46 +0900

    Fix broken tests: add missing board argument to next_possible_coordinates(..).

--- core/tests/test_rule.py
+++ core/tests/test_rule.py
@@ -1,9 +1,17 @@
+import pytest
+
 from data import Piece
+from janggi import Janggi
 from rule import next_possible_coordinates
 
 
-def test_cha_mobility():
-    coords = set(next_possible_coordinates(1, 1, Piece.Cha_a))
+@pytest.fixture(scope='function')
+def empty_board():
+    return Janggi().board
+
+
+def test_cha_mobility(empty_board):
+    coords = set(next_possible_coordinates(empty_board, 1, 1, Piece.Cha_a))
 
     up = {(0, 1)}
     down = {(2, 1), (3, 1), (4, 1), (5, 1), (6, 1), (7, 1), (8, 1), (9, 1)}
@@ -12,15 +20,15 @@ def test_cha_mobility():
     assert coords == left | right | up | down
 
 
-def test_byung_mobility():
-    coords = set(next_possible_coordinates(2, 2, Piece.Byung_a))
+def test_byung_mobility(empty_board):
+    coords = set(next_possible_coordinates(empty_board, 2, 2, Piece.Byung_a))
 
     down, left, right = (3, 2), (2, 1), (2, 3)
     assert coords == {down, left, right}
 
 
-def test_jol_mobility():
-    coords = set(next_possible_coordinates(6, 2, Piece.Jol_b))
+def test_jol_mobility(empty_board):
+    coords = set(next_possible_coordinates(empty_board, 6, 2, Piece.Jol_b))
 
     up, left, right = (5, 2), (6, 1), (6, 3)
     assert coords == {up, left, right}
